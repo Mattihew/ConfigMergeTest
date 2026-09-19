3397597bcc992daf427bb6974aabc8aaa84313e4
Mattihew <me@mattihew.com>
2026-09-19 14:23:15 +0100

    changes offset to dictonary per path

--- ConfigMergeTest/ConfigMerger/ConfigMergerProvider.cs
+++ ConfigMergeTest/ConfigMerger/ConfigMergerProvider.cs
@@ -5,7 +5,7 @@ namespace ConfigMergeTest.ConfigMerger;
 
 public class ConfigMergerProvider(IEnumerable<string> arrayPaths, IConfigurationProvider provider) : IConfigurationProvider
 {
-    private int _offset = 0;
+    private readonly Dictionary<string, int> _offsets = new();
 
     public IChangeToken GetReloadToken() => provider.GetReloadToken();
     public void Load() => provider.Load();
@@ -24,7 +24,8 @@ public class ConfigMergerProvider(IEnumerable<string> arrayPaths, IConfiguration
         }
 
         var parts = key.Substring(arrayPath.Length + 1).Split(':', 2);
-        parts[0] = int.TryParse(parts[0], out var indexValue) ? (indexValue - _offset).ToString() : parts[0];
+        var offset = _offsets.GetValueOrDefault(arrayPath, 0);
+        parts[0] = int.TryParse(parts[0], out var indexValue) ? (indexValue - offset).ToString() : parts[0];
         return arrayPath + ':' + string.Join(':', parts);
     }
 
@@ -43,11 +44,12 @@ public class ConfigMergerProvider(IEnumerable<string> arrayPaths, IConfiguration
         }
         
         var keys = earlierKeys.ToList();
-        _offset = TryParse(keys).Max() + 1 ?? 0;
+        var offset = TryParse(keys).Max() + 1 ?? 0;
+        _offsets[arrayPath] = offset;
 
         var result = provider.GetChildKeys(Enumerable.Empty<string>(), parentPath);
 
-        return keys.Concat(result.Select(key => int.TryParse(key, out var value) ? (value + _offset).ToString() : key));
+        return keys.Concat(result.Select(key => int.TryParse(key, out var value) ? (value + offset).ToString() : key));
     }
 
     private IEnumerable<int?> TryParse(IEnumerable<string> input) =>
